2822a6b6f99d014dd870f92c61dd0ae12a545634
alebianky <alebianky@gmail.com>
2021-04-09 11:51:47 -0300

    primer commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,50 @@
+// RAFC
+
+import React, {useState} from 'react'
+import PropTypes from 'prop-types';
+
+export const AddCategory = ( {setCategories } ) => {
+
+
+    const [inputValue , setInputValue] =  useState('');
+
+    const handleInput =(e) => {
+
+     // console.log(e.target.value)
+       setInputValue(e.target.value)
+      // console.log(e.target.value)
+    }
+
+   const handleSubmit = (e) => {
+            e.preventDefault();
+            
+            setCategories( cats => [ inputValue, ...cats ])
+
+          //  console.log('Submit Hecho');
+            setInputValue('');
+    }
+
+    const handleFocus = () => {
+        setInputValue('');
+    }
+
+
+    return (
+        <>
+        <form onFocus={ handleFocus} onSubmit={handleSubmit}>
+                {/* <h2>{ inputValue}</h2> */}
+                <input 
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInput}
+                    />
+        </form>
+        </>
+    )
+}
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired ,
+    
+}
